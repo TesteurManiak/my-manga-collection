df893dafdfdda6fda0d708c8dc210e0afda627ef
Guillaume Roux <rouxguillaume8@gmail.com>
2022-06-08 16:37:43 +0200

    updated screenshots

--- lib/presentation/views/common/owned_widget.dart
+++ lib/presentation/views/common/owned_widget.dart
@@ -17,17 +17,22 @@ class OwnedWidget extends StatelessWidget {
     final theme = Theme.of(context);
     final appTextStyles = theme.extension<AppTextStyles>();
     final appColors = theme.extension<AppColors>();
+
+    final completed = total != null && total! > 0 && owned == total;
+    final color = completed ? Colors.lightGreen : appColors?.ownedCounter;
+    final style = appTextStyles?.ownedCounter?.copyWith(color: color);
+
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         Text(
           total == null || total == 0 ? '?' : '$owned/$total',
-          style: appTextStyles?.ownedCounter,
+          style: style,
         ),
         const SizedBox(width: 4),
         Transform.rotate(
           angle: math.pi / 14,
-          child: Icon(Icons.book, color: appColors?.ownedCounter),
+          child: Icon(Icons.book, color: color),
         ),
       ],
     );
